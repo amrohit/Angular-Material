0c734e28b64004ecec94f4e9d0a2a51fd3a30e27
Rohit Sharma <amrohit@yahoo.com>
2019-02-19 01:08:42 +0530

    Angular mat

---
+++ 2-Diving Deeper into Angular Material/1-Adding Navigation & a Sidenav/app/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatSidenavModule
+} from "@angular/material";
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule
+  ]
+})
+export class MaterialModule {}
